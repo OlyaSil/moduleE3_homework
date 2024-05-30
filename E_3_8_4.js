0d3badab48a95be6e25666922ccfde961cf027a7
OlyaSil <150305029+OlyaSil@users.noreply.github.com>
2024-05-30 20:49:02 +0200

    Create E_3_8_4.js
    Task 4 uploaded

---
+++ E_3_8_4.js
@@ -0,0 +1,13 @@
+function printNumbers(start, end) {
+    let current = start;
+    const intervalId = setInterval(() => {
+        console.log(current);
+        if (current === end) {
+            clearInterval(intervalId);
+        }
+        current++;
+    }, 1000);
+}
+
+// Пример использования:
+printNumbers(5, 15);
